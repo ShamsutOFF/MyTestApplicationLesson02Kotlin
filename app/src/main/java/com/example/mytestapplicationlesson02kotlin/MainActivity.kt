78b586c5195d13c06a0f9cb2f3c23b8d9a2e1414
ShamsutOFF <shamsutoff@yandex.ru>
2021-07-27 14:52:59 +0300

    1

--- app/src/main/java/com/example/mytestapplicationlesson02kotlin/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplicationlesson02kotlin/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.example.mytestapplicationlesson02kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.mytestapplicationlesson02kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var toastButton: Button
+    private lateinit var subscribeButton: Button
+    private lateinit var unsubscribeButton: Button
+    private lateinit var inputEditText: EditText
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val model = Model()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)  //Binding
+        setContentView(binding.root)
 
-        toastButton = findViewById(R.id.toast_button)
+        subscribeButton = findViewById(R.id.subscribe_button)
+        unsubscribeButton = findViewById(R.id.unsubscribe_button)
+        inputEditText = findViewById(R.id.input_edit_text)
+
+        inputEditText.addTextChangedListener{
+            model.setValue(it.toString())
+        }
+
+        subscribeButton.setOnClickListener {
+            model.subscriber = {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+        unsubscribeButton.setOnClickListener {
+            model.subscriber = null
+        }
     }
 }
--- app/src/main/java/com/example/mytestapplicationlesson02kotlin/Model.kt
+++ app/src/main/java/com/example/mytestapplicationlesson02kotlin/Model.kt
@@ -1,13 +1,15 @@
-package com.example.mytestapplicationlesson02kotlin;
+package com.example.mytestapplicationlesson02kotlin
 
-public class Model {
-    private var importantString: String = ""
+class Model {
+    var subscriber: ((string: String?) -> Unit)? = null
+    private var importantString: String? = null
 
-    fun setValue(value: String) {
+    fun setValue(value: String?) {
         importantString = value
+        subscriber?.invoke(value)
     }
 
-    fun getValue ():String{
+    fun getValue(): String? {
         return importantString
     }
 }
